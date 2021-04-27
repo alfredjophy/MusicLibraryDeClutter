4fa34a72682a9d98af303a737be30b67a44de05a
AlfredEVOL <57448041+AlfredEVOL@users.noreply.github.com>
2021-04-28 01:18:09 +0530

    done

--- songsort.sh
+++ songsort.sh
@@ -3,11 +3,29 @@
 inputDirectory=$1
 
 for filename in $inputDirectory/*.mp3; do
-	songTitle=$(songrec audio-file-to-recognized-song "$filename"|python3 ./jsonParser.py )
+	songTitle=$(songrec audio-file-to-recognized-song "$filename"|python3 ~/Project/SongSorter/jsonParser.py )
 	echo $songTitle
-	if [ ! -d "$inputDirectory/$songTitle" ]; then
-		mkdir "$inputDirectory/$songTitle"
+	dirName=${songTitle// /_}
+	dirName=${dirName//(}
+	dirName=${dirName//)}
+	dirName=${dirName//\"}
+	dirName=${dirName//./_}
+	if [ ! -d "${dirName}" ]; then
+		mkdir "${dirName}"
+		echo "$songTitle" >> "$dirName"/songName.txt
 	fi
+	newfilename=${filename// /_}
+	mv "$filename" "$newfilename"
+	mv "$newfilename" "${dirName}"
+done
+
+for subDir in "$inputDirectory"/*/
+	do
+	largestFile=$(find "$subDir" -type f -exec ls -al {} \; | sort -nr -k5 | head -n 1|awk '{print $9}')
+	echo $largestFile
+	
+	filename=$(cat "$inputDirectory"/"$subDir"/songName.txt)
 	echo $filename
-	cp "$filename" "$inputDirectory/$songTitle/"
+	mv "$largestFile" "$inputDirectory/$filename.mp3"
+	rm -rf "$subDir"
 done
